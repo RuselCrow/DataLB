bf36d46e72611858e0d53ea6a07d03947bb6d552
foch <dvchervakov@gmail.com>
2018-12-24 11:58:59 +0300

    fixes

--- app/components/main.js
+++ app/components/main.js
@@ -2,12 +2,11 @@ import './fonts/fonts.less'
 import '../libs/bootstrap/bootstrap-grid-3.3.1.min.css'
 import '../libs/particleground-master/jquery.particleground.js'
 import '../libs/particleground-master/demo/js/jquery-1.11.1.min.js'
+
 //import 'bootstrap'
 
 
-document.getElementById('toggle-hamburger').onclick=function(){
-    onHamburger();
-}
+
 function onHamburger() {
     const hamburger = document.getElementById('toggle-hamburger');
     const menu = document.getElementById('side-menu');
@@ -22,30 +21,20 @@ function onHamburger() {
     }
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-  particleground(document.getElementById('particles'), {
-    dotColor: 'white',
-    lineColor: 'white',
-    directionY: 'up',
-    particleRadius: '20',
-    lineWidth:'7',
-    maxSpeedY:'0',
-    offsetY:'3000',
-    density:'10000'
-
-  });
-}, false);
-
-document.addEventListener('DOMContentLoaded', function () {
-  particleground(document.getElementById('particles1'), {
-    dotColor: '#333333',
-    lineColor: '#333333',
-    directionY: 'up',
-    particleRadius: '30',
-    lineWidth:'8',
-    maxSpeedY:'0',
-    offsetY:'4000',
-    density:'10000'
-
-  });
-}, false);
+function emitEvent(name){
+  if (yaCounter50530423 && (typeof yaCounter50530423.reachGoal === 'function')) {
+      yaCounter50530423.reachGoal(name);
+    }
+}
+document.addEventListener("DOMContentLoaded", function(event) {
+  document.getElementById('button1').onclick=()=>{emitEvent('button1');changeTexts('"ИППОДРОМ МАНЕЖ"',"20:00-22:00");};
+  document.getElementById('button2').onclick=()=>{emitEvent('button2');changeTexts('"ВАТАН"',"19:00-21:00");};
+  document.getElementById('button3').onclick=()=>{emitEvent('button3');changeTexts('"ТАСМА"',"18:30-20:30");};
+  document.getElementById('submit').onclick=()=>{emitEvent('send');};
+});
+function changeTexts(name,time){
+  var nameElem=document.getElementById("modal-name");
+  nameElem.innerText=name;
+  var timeElem=document.getElementById("modal-time");
+  timeElem.innerText=time;
+}
--- app/pages/index/index.js
+++ app/pages/index/index.js
@@ -2,3 +2,4 @@ import './index.less';
 import '../../components/main'
 import '../../components/header/header'
 import '../../components/footer/footer'
+
