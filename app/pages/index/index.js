a2f9be618f46be8d091780588b9af0e32c2056ae
tarasov.a <tarasov.art.ser@gmail.com>
2019-04-25 21:20:42 +0300

    remove animation in js

--- app/pages/index/index.js
+++ app/pages/index/index.js
@@ -1,34 +1,7 @@
 import './index.less';
-import '../../components/main'
-import '../../components/header/header'
+import '../../components/header/header';
 import '../../components/footer/footer';
-import '../../libs/jquery-ui-1.12.1.custom/jquery-ui'
-import '../../libs/jquery-ui-1.12.1.custom/jquery-ui.css'
+import '../../libs/jquery-ui-1.12.1.custom/jquery-ui';
+import '../../libs/jquery-ui-1.12.1.custom/jquery-ui.css';
 
-var lefftFoot = document.getElementById('left-foot');
-var footAnim = false;
-setInterval(() => {
-    if (footAnim) {
-        lefftFoot.classList.add('active');
-    } else {
-        if (lefftFoot.classList.contains('active')) {
-            lefftFoot.classList.remove('active');
-        }
-    }
-    footAnim = !footAnim;
-}, 6000);
-
-// var mySVG = document.getElementById('svg');
-// mySVG.setAttribute("viewBox", "0 `0 8 29");
-// console.log($("#svg"));
-
-// $("#svg").animate({
-//     viewBox: "0 10 8 29"
-// })
-
-
-
-// function animViewbox (elements, complete, remaining, start, tweenValue) {
-//      elements[0].setAttribute('viewBox', '0 0 ' + tweenValue + ' ' + tweenValue);  
-// }
 
