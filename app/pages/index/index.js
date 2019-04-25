daeb67a812cf06c5e28b334f2d8a4261b157e236
tarasov.a <tarasov.art.ser@gmail.com>
2019-04-25 19:28:11 +0300

    animations

--- app/pages/index/index.js
+++ app/pages/index/index.js
@@ -5,6 +5,18 @@ import '../../components/footer/footer';
 import '../../libs/jquery-ui-1.12.1.custom/jquery-ui'
 import '../../libs/jquery-ui-1.12.1.custom/jquery-ui.css'
 
+var lefftFoot = document.getElementById('left-foot');
+var footAnim = false;
+setInterval(() => {
+    if (footAnim) {
+        lefftFoot.classList.add('active');
+    } else {
+        if (lefftFoot.classList.contains('active')) {
+            lefftFoot.classList.remove('active');
+        }
+    }
+    footAnim = !footAnim;
+}, 6000);
 
 // var mySVG = document.getElementById('svg');
 // mySVG.setAttribute("viewBox", "0 `0 8 29");
