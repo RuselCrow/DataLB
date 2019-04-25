f19e391490d1fe5162ea24f23a736a4ae7055269
tarasov.a <tarasov.art.ser@gmail.com>
2019-04-25 17:31:23 +0300

    animation

--- app/pages/index/index.js
+++ app/pages/index/index.js
@@ -1,5 +1,22 @@
 import './index.less';
 import '../../components/main'
 import '../../components/header/header'
-import '../../components/footer/footer'
+import '../../components/footer/footer';
+import '../../libs/jquery-ui-1.12.1.custom/jquery-ui'
+import '../../libs/jquery-ui-1.12.1.custom/jquery-ui.css'
+
+
+// var mySVG = document.getElementById('svg');
+// mySVG.setAttribute("viewBox", "0 `0 8 29");
+// console.log($("#svg"));
+
+// $("#svg").animate({
+//     viewBox: "0 10 8 29"
+// })
+
+
+
+// function animViewbox (elements, complete, remaining, start, tweenValue) {
+//      elements[0].setAttribute('viewBox', '0 0 ' + tweenValue + ' ' + tweenValue);  
+// }
 
